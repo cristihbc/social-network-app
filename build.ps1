e9f5cbef632b7d79e9914bc2b40c4fd70e7d5b74
Alex Cojocaru <alex.cojocaru73@gmail.com>
2021-04-10 00:18:51 +0300

    [windows] tool to build and run the docker container

---
+++ build.ps1
@@ -0,0 +1,19 @@
+# Small tool to build the app automatically on windows
+# Usage
+# build.ps1 - to build and run the app
+# build.ps1 run - only to run the app
+# 
+# The app is deployed to localhost:5001
+
+if ( $null -eq $args[0] ) {
+    Write-Output "Starting docker build..."
+    Start-Sleep -s 1
+
+    docker build -t socialapp .
+    Write-Output "Finished the build..." 
+}
+
+Write-Output "Running on port 5001"
+docker run -p 5001:5001 -e PORT=5001 -d socialapp
+
+Write-Output "Deployed the app..."

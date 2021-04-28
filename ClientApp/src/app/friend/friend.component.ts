349888eb0f9ad03783030e6002a0d2a1f0d453a9
Parascheva Negru <parascheva.negru@gmail.com>
2021-04-28 22:36:12 +0300

    friend component

--- ClientApp/src/app/friend/friend.component.ts
+++ ClientApp/src/app/friend/friend.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-friend',
   templateUrl: './friend.component.html',
   styleUrls: ['./friend.component.css']
 })
-export class FriendComponent implements OnInit {
+export class FriendComponent {
+  public friends: Friend[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Friend[]>(baseUrl + 'api/friend').subscribe(result => {
+      this.friends = result;
+    }, error => console.error(error));
   }
 
+}
+
+interface Friend {
+  username: string;
+  friendUsername: string;
+  friendshipDate: string;
 }

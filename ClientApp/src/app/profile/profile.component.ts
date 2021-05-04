0492159d290eb441274e4f006c53077de332c35f
Alex Cojocaru <alex.cojocaru73@gmail.com>
2021-05-04 18:32:37 +0300

    friends window

--- ClientApp/src/app/profile/profile.component.ts
+++ ClientApp/src/app/profile/profile.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BurritoService } from '../burrito.service';
+import { User } from '../users/user';
 
 @Component({
   selector: 'app-profile',
@@ -8,21 +11,48 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
   username: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  phone: string;
+  user: User;
+  isLogged: boolean;
+  friends: Friend[];
+  
+  constructor(
+    private route: ActivatedRoute, 
+    private http: HttpClient,
+    private burritoService: BurritoService,
+    @Inject('BASE_URL') baseUrl: string
+    ) { 
+    this.isLogged = this.burritoService.getLoggedInfo();
+    
+    if (this.isLogged) {
+      http.get<User[]>(baseUrl + 'api/user').subscribe(result => {
+        this.user = result.filter(it => it.username === this.username)[0];
+      });
 
-  constructor(private route: ActivatedRoute) { 
-    this.username = null;
-    this.firstName = 'Alex';
-    this.lastName = 'Cojocaru';
-    this.email = 'alex.cojocaru73@gmail.com';
-    this.phone = '214348234';
+      http.get<Friend[]>(baseUrl + 'api/friend').subscribe(result => {
+        this.friends = result;
+      });
+    }
   }
 
   ngOnInit() {
     this.username = this.route.snapshot.paramMap.get('username');
   }
+}
 
+// interface User {
+//   username: string;
+//   firstName: string;
+//   lastName: string;
+//   email: string;
+//   phone: string;
+//   password: string;
+//   sex: number;
+//   city: string;
+//   country: string;
+// }
+
+interface Friend {
+  username: string;
+  friendUsername: string;
+  friendshipData: string;
 }

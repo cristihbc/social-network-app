b3d228d909f2aaff68844406782612dc6bd60193
BogdanCosminBarbu <63199550+BogdanCosminBarbu@users.noreply.github.com>
2021-05-05 14:22:17 +0300

    Post stuff (#17)
    * Post stuff
    
    * new posts
    
    * posts v2
    
    * post v3
    
    Co-authored-by: barbubogdancosmin <bogdan-cosmin.barbu@student.tuiasi.ro>

---
+++ Models/PostEntity.cs
@@ -0,0 +1,36 @@
+/**************************************************************************
+ *                                                                        *
+ *  File:        PostEntity.cs                                            *
+ *  Copyright:   (c) 2021, Barbu Bogdan-Cosmin                            *
+ *  E-mail:      barbubogdan1337@gmail.com                                *
+ *  Description: The description of a post entity sent inside a request   * 
+ *               body                                                     *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or modify  *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation. This program is distributed in the      *
+ *  hope that it will be useful, but WITHOUT ANY WARRANTY; without even   *
+ *  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR   *
+ *  PURPOSE. See the GNU General Public License for more details.         *
+ *                                                                        *
+ **************************************************************************/
+
+using Newtonsoft.Json;
+
+namespace Models
+{
+    public class PostEntity
+    {
+        [JsonProperty("id")]
+        public uint Id { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+
+        [JsonProperty("date")]
+        public string Date { get; set; }
+    }
+}
